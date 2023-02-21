272bc0454231083e6ef7e6206cb2e8090340c57e
rd1r0cc0 <39325791+rd1r0cc0@users.noreply.github.com>
2023-02-21 01:13:15 -0600

    Remove ToDo Item - Update
    Module/Video 14.6 - created way to remove items from the ToDo list

--- index.js
+++ index.js
@@ -24,10 +24,16 @@ function App(){
     setValue('');
 
   }
+  const removeTodo = e => {
+    const index = Number(e.target.id);
+    let  temp = [...todos];
+    temp.splice(index,1);
+    setTodos(temp);
+  }
 
   return (<>
     {todos.map((todo,i) =>
-      <div className="todo" key={i}>{todo.text}</div>)}
+      <div className="todo" key={i} id={i} onClick={removeTodo}>{todo.text}</div>)}
       <form onSubmit={handleSubmit}>
         <input 
           type='text'
